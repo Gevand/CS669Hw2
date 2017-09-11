8cf878f61d538b4f7f36bdb1963d181bb6f0750d
Gevand <gevand@outlook.com>
2017-09-10 20:33:19 -0700

    Homework completed.

--- CS669GevandBalayanHomework2/Form1.Designer.cs
+++ CS669GevandBalayanHomework2/Form1.Designer.cs
@@ -28,20 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.pictInput = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictInput)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(12, 3);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(100, 50);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Open Picture";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // pictInput
+            // 
+            this.pictInput.Location = new System.Drawing.Point(12, 60);
+            this.pictInput.Name = "pictInput";
+            this.pictInput.Size = new System.Drawing.Size(579, 564);
+            this.pictInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictInput.TabIndex = 1;
+            this.pictInput.TabStop = false;
+            this.pictInput.Click += new System.EventHandler(this.pictInput_Click);
+            this.pictInput.Paint += new System.Windows.Forms.PaintEventHandler(this.pictInput_Paint);
+            this.pictInput.MouseEnter += new System.EventHandler(this.pictInput_MouseEnter);
+            this.pictInput.MouseLeave += new System.EventHandler(this.pictInput_MouseLeave);
+            this.pictInput.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictInput_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1062, 861);
+            this.Controls.Add(this.pictInput);
+            this.Controls.Add(this.btnUpload);
             this.Name = "Form1";
             this.Text = "Gevand Balayan -- Homework 2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictInput)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.PictureBox pictInput;
     }
 }
 
--- CS669GevandBalayanHomework2/Form1.cs
+++ CS669GevandBalayanHomework2/Form1.cs
@@ -15,6 +15,150 @@ namespace CS669GevandBalayanHomework2
         public Form1()
         {
             InitializeComponent();
+            string fileName = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"images\test.jpg");
+            if (System.IO.File.Exists(fileName))
+            {
+                pictInput.Image = new Bitmap(fileName);
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                using (OpenFileDialog dlg = new OpenFileDialog())
+                {
+                    dlg.Title = "Open Image";
+                    dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+                    if (dlg.ShowDialog() == DialogResult.OK)
+                    {
+                        pictInput.Image = new Bitmap(dlg.FileName);
+
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error! Couldn't load image.");
+            }
+        }
+
+        private void pictInput_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            Point coordinates = me.Location;
+        }
+        float boxSize = 8;
+        private void pictInput_Paint(object sender, PaintEventArgs e)
+        {
+            DoubleBuffered = true;
+            Point local = this.PointToClient(Cursor.Position);
+            e.Graphics.DrawRectangle(Pens.Black, local.X - 15, local.Y - 65, boxSize, boxSize);
+            btnUpload.Refresh();
+            pictInput.Invalidate();
+
+        }
+
+        private void pictInput_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+        }
+
+        private void pictInput_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Show();
+        }
+
+        private void pictInput_MouseUp(object sender, MouseEventArgs e)
+        {
+            var bitMap = new Bitmap(pictInput.Image);
+            try
+            {
+                Color?[,] pixels = new Color?[8, 8];
+                for (int i = 0; i < 8; i++)
+                    for (int j = 0; j < 8; j++)
+                    {
+                        var myX = 0;
+                        var myY = 0;
+
+                        if (i > 4)
+                            myX = e.X + (i - 4);
+                        if (i <= 4)
+                            myX = e.X - (-1 * i + 4);
+                        if (j > 4)
+                            myY = e.Y + (j - 4);
+                        if (j <= 4)
+                            myY = e.Y - (-1 * j + 4);
+
+                        if (myX > 0 && myY > 0 && myX < bitMap.Width && myY < bitMap.Height)
+                        {
+                            var pixel = bitMap.GetPixel(myX, myY);
+                            pixels[i, j] = Color.FromArgb(pixel.R, pixel.G, pixel.B);
+
+
+                        }
+                        else
+                        {
+                            pixels[i, j] = null;
+                        }
+                    }
+                GenerateNewForm(pixels, "Red");
+                GenerateNewForm(pixels, "Green");
+                GenerateNewForm(pixels, "Blue");
+                Form tempForm = new Form();
+                tempForm.Height = 200;
+                tempForm.Width = 200;
+                tempForm.Text = "Image";
+                PictureBox tempBox = new PictureBox();
+                tempBox.Height = 200;
+                tempBox.Width = 200;
+                Bitmap tempBitMap = new Bitmap(8, 8);
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (pixels[i, j].HasValue)
+                            tempBitMap.SetPixel(i, j, pixels[i, j].Value);
+                    }
+                }
+                tempBox.Image = tempBitMap;
+                tempBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                tempForm.Controls.Add(tempBox);
+                tempForm.Show();
+            }
+            catch { MessageBox.Show("Could not get pixel information"); }
+        }
+        private void GenerateNewForm(Color?[,] pixels, string title)
+        {
+
+            Form tempForm = new Form();
+            tempForm.Height = 200;
+            tempForm.Width = 200;
+            tempForm.Text = title;
+            Label myView = new Label();
+            myView.Height = tempForm.Height;
+            myView.Width = tempForm.Width;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (pixels[i, j].HasValue)
+                        if (title == "Red")
+                            myView.Text += pixels[i, j].Value.R + " ";
+                        else if (title == "Green")
+                            myView.Text += pixels[i, j].Value.G + " ";
+                        else
+                            myView.Text += pixels[i, j].Value.B + " ";
+                    else
+                        myView.Text += "N";
+                }
+                myView.Text += Environment.NewLine;
+            }
+            tempForm.Controls.Add(myView);
+            tempForm.Show();
+
         }
     }
 }
